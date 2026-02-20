687b0edc12ff31f41b9253d8905e33415fb1860c
DuongDucBinh <21011878@st.phenikaa-uni.edu.vn>
2026-02-21 00:08:49 +0700

    start project

---
+++ src/main/java/com/sm/demo/entity/Student.java
@@ -0,0 +1,15 @@
+package com.sm.demo.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String address;
+}
